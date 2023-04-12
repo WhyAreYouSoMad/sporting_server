5d9aed8b03a3017bda0b2cfc50021bbf5dcb082b
aozp73 <aozp73@naver.com>
2023-04-12 09:33:17 +0900

    initial commit

---
+++ Desktop/sporting/sporting_server/src/test/java/shop/mtcoding/sporting_server/SportingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.sporting_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
