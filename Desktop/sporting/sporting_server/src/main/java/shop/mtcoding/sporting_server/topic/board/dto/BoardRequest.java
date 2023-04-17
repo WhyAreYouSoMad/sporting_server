57e7f368a7f51a057ec21a51b3293c19a12249a7
aozp73 <aozp73@naver.com>
2023-04-17 14:38:14 +0900

    GFP-76-Controller Mock테스트 완료

--- Desktop/sporting/sporting_server/src/main/java/shop/mtcoding/sporting_server/topic/board/dto/BoardRequest.java
+++ Desktop/sporting/sporting_server/src/main/java/shop/mtcoding/sporting_server/topic/board/dto/BoardRequest.java
@@ -1,23 +1,5 @@
 package shop.mtcoding.sporting_server.topic.board.dto;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import shop.mtcoding.sporting_server.core.enums.ValueOfEnum;
-import shop.mtcoding.sporting_server.core.enums.field.etc.StadiumAddress;
-
 public class BoardRequest {
 
-    @Getter
-    @Setter
-    @NoArgsConstructor
-    @EqualsAndHashCode
-    public static class StadiumRegistrationInDTO {
-        private String name;
-
-        @ValueOfEnum(enumClass = StadiumAddress.class, message = "공지사항 상태 값이 이상이 있습니다. 확인해주세요.")
-        private String address;
-        private String category;
-    }
 }
--- Desktop/sporting/sporting_server/src/main/java/shop/mtcoding/sporting_server/topic/stadium/dto/StadiumResponse.java
+++ Desktop/sporting/sporting_server/src/main/java/shop/mtcoding/sporting_server/topic/stadium/dto/StadiumResponse.java
@@ -1,5 +1,6 @@
 package shop.mtcoding.sporting_server.topic.stadium.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ public class StadiumResponse {
     @Getter
     @Setter
     @NoArgsConstructor
+    @AllArgsConstructor
     @EqualsAndHashCode
     public static class StadiumRegistrationOutDTO {
         private Long id;
