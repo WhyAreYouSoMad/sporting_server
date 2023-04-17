28cf90718742436a08fb0bc7bdc1cc6977dc61b3
aozp73 <aozp73@naver.com>
2023-04-17 14:04:12 +0900

    GFP-76-Service 진행 완료

--- Desktop/sporting/sporting_server/src/main/java/shop/mtcoding/sporting_server/topic/stadium/StadiumService.java
+++ Desktop/sporting/sporting_server/src/main/java/shop/mtcoding/sporting_server/topic/stadium/StadiumService.java
@@ -22,5 +22,37 @@ import shop.mtcoding.sporting_server.topic.stadium.dto.StadiumResponse.StadiumRe
 public class StadiumService {
 
     private final StadiumRepository stadiumRepository;
+    private final CompanyInfoRepository companyInfoRepository;
+    private final SportCategoryRepository sportCategoryRepository;
+
+    // 모킹용
+    private final UserRepository userRepository;
+
+    @Transactional
+    public StadiumRegistrationOutDTO save(Long id, StadiumRequest.StadiumRegistrationInDTO stadiumRegistrationInDTO) {
+
+        // 모킹용
+        // (company topic에서 info 저장 구현한다면, User 찾아서 User.companyinfo를 Stadium에
+        // 저장하는 코드로 변경예정)
+        User userPS = userRepository.findById(id).orElseThrow(() -> {
+            throw new Exception400("해당 유저정보가 존재하지 않습니다.");
+        });
+        CompanyInfo companyInfo = CompanyInfo
+                .builder()
+                .user(userPS)
+                .build();
+        CompanyInfo companyInfoPS = companyInfoRepository.save(companyInfo);
+
+        // 진행
+        SportCategory sportCategoryPS = sportCategoryRepository.findBySport(stadiumRegistrationInDTO.getCategory())
+                .orElseThrow(() -> {
+                    throw new Exception400("해당 스포츠 카테고리가 존재하지 않습니다.");
+                });
+
+        Stadium stadiumPS = stadiumRepository
+                .save(stadiumRegistrationInDTO.toEntity(companyInfoPS, stadiumRegistrationInDTO, sportCategoryPS));
+
+        return new StadiumRegistrationOutDTO(stadiumPS);
+    }
 
 }
