d2870574becd9853a2f184c7abeec12e33f76457
jiyeon950510 <siki4682@naver.com>
2023-04-20 20:14:15 +0900

    컴퍼티 업데이트 폼 완료!

--- Desktop/sporting/sporting_server/src/main/java/shop/mtcoding/sporting_server/modules/company_info/repository/CompanyInfoCustomRepository.java
+++ Desktop/sporting/sporting_server/src/main/java/shop/mtcoding/sporting_server/modules/company_info/repository/CompanyInfoCustomRepository.java
@@ -1,5 +1,10 @@
 package shop.mtcoding.sporting_server.modules.company_info.repository;
 
+import org.springframework.data.repository.query.Param;
+
+import shop.mtcoding.sporting_server.topic.company.dto.CompanyInfoResponseDTO;
+
 public interface CompanyInfoCustomRepository {
+    CompanyInfoResponseDTO findCompanyInfoByUserId(@Param("id") Long id);
 
 }
