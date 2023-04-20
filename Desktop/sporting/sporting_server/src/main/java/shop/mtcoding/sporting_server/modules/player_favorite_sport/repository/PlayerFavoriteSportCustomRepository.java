c2c0221851217198ec8a6799ca9b75b9f8563ed1
Limwonbin <wonbin3366@naver.com>
2023-04-20 10:44:48 +0900

    GFP-92-Controller,Service,Repository 완료

--- Desktop/sporting/sporting_server/src/main/java/shop/mtcoding/sporting_server/modules/player_favorite_sport/repository/PlayerFavoriteSportCustomRepository.java
+++ Desktop/sporting/sporting_server/src/main/java/shop/mtcoding/sporting_server/modules/player_favorite_sport/repository/PlayerFavoriteSportCustomRepository.java
@@ -1,5 +1,11 @@
 package shop.mtcoding.sporting_server.modules.player_favorite_sport.repository;
 
-public interface PlayerFavoriteSportCustomRepository {
+import java.util.List;
 
+import org.springframework.data.repository.query.Param;
+
+import shop.mtcoding.sporting_server.topic.player.dto.PlayerFavoriteSportResponseDTO;
+
+public interface PlayerFavoriteSportCustomRepository {
+    List<PlayerFavoriteSportResponseDTO> findSportByuserId(@Param("userId") Long userId);
 }
