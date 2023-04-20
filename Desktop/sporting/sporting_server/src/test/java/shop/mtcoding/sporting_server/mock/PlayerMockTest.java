01b41ab7daff6dd847dd7ba7b0abf5ff7e2c6177
Limwonbin <wonbin3366@naver.com>
2023-04-20 11:28:43 +0900

    GFP-92-Controller Mock 테스트 완료

--- Desktop/sporting/sporting_server/src/test/java/shop/mtcoding/sporting_server/mock/PlayerMockTest.java
+++ Desktop/sporting/sporting_server/src/test/java/shop/mtcoding/sporting_server/mock/PlayerMockTest.java
@@ -2,24 +2,42 @@ package shop.mtcoding.sporting_server.mock;
 
 import static org.mockito.BDDMockito.given;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.http.MediaType;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import shop.mtcoding.sporting_server.core.auth.MyUserDetails;
+import shop.mtcoding.sporting_server.core.enums.field.etc.PlayerInfoAddress;
+import shop.mtcoding.sporting_server.core.enums.field.etc.PlayerInfoAge;
+import shop.mtcoding.sporting_server.core.enums.field.etc.PlayerInfoGender;
+import shop.mtcoding.sporting_server.core.jwt.MyLoginUser;
 import shop.mtcoding.sporting_server.topic.player.PlayerController;
 import shop.mtcoding.sporting_server.topic.player.PlayerService;
+import shop.mtcoding.sporting_server.topic.player.dto.PlayerFavoriteSportResponseDTO;
+import shop.mtcoding.sporting_server.topic.player.dto.PlayerInfoResponseDTO;
 import shop.mtcoding.sporting_server.topic.player.dto.PlayerRequest;
 import shop.mtcoding.sporting_server.topic.player.dto.PlayerResponse;
+import shop.mtcoding.sporting_server.topic.player.dto.PlayerUpdateFormOutDTO;
 
 @WebMvcTest(PlayerController.class)
 public class PlayerMockTest {
@@ -32,6 +50,17 @@ public class PlayerMockTest {
 
         @Autowired
         private ObjectMapper objectMapper;
+
+        @BeforeEach
+        public void init() {
+                MyLoginUser user = MyLoginUser.builder().id(1L).role("USER").build();
+                MyUserDetails myUserDetails = new MyUserDetails(user);
+                Authentication authentication = new UsernamePasswordAuthenticationToken(
+                                myUserDetails,
+                                myUserDetails.getPassword(),
+                                myUserDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
 
         @Test
         @WithMockUser(username = "cos", roles = { "User" })
@@ -59,5 +88,45 @@ public class PlayerMockTest {
                 resultActions
                                 .andExpect(status().isOk())
                                 .andExpect(jsonPath("$.data.email").value("ssar@nate.com"));
+        }
+
+        @Test
+        @WithMockUser(username = "cos", roles = { "User" })
+        @DisplayName("플레이어 정보 수정 페이지 테스트")
+        void playerUpdateFormTest() throws Exception {
+                // given
+                Long userId = 1L;
+                PlayerUpdateFormOutDTO playerUpdateFormOutDTO = new PlayerUpdateFormOutDTO(1L, "ssar123",
+                                "ssar@nate.com", "1234");
+
+                List<PlayerFavoriteSportResponseDTO> PlayerFavoriteSportResponseListDTO = new ArrayList();
+                PlayerFavoriteSportResponseDTO PlayerFavoriteSportResponseDTO1 = new PlayerFavoriteSportResponseDTO(1L,
+                                "축구");
+                PlayerFavoriteSportResponseDTO PlayerFavoriteSportResponseDTO2 = new PlayerFavoriteSportResponseDTO(2L,
+                                "농구");
+                PlayerFavoriteSportResponseListDTO.add(PlayerFavoriteSportResponseDTO1);
+                PlayerFavoriteSportResponseListDTO.add(PlayerFavoriteSportResponseDTO2);
+                playerUpdateFormOutDTO.setPlayerFavoriteSport(PlayerFavoriteSportResponseListDTO);
+
+                PlayerInfoResponseDTO playerInfoResponseDTO = new PlayerInfoResponseDTO(1L, "010-1001-1111",
+                                PlayerInfoGender.남자, PlayerInfoAge.AGE_20,
+                                PlayerInfoAddress.부산시);
+                playerUpdateFormOutDTO.setPlayerInfo(playerInfoResponseDTO);
+
+                given(this.playerService.getUpdateForm(userId, userId))
+                                .willReturn(playerUpdateFormOutDTO);
+
+                // When
+                ResultActions resultActions = this.mvc.perform(
+                                get("/api/user/updateform/" + userId)
+                                                .with(csrf()));
+
+                String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+                System.out.println("테스트 : " + responseBody);
+
+                // Then
+                resultActions
+                                .andExpect(status().isOk())
+                                .andExpect(jsonPath("$.data.nickname").value("ssar123"));
         }
 }
