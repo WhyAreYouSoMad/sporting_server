9457f419046231309d08543222ab889eb668ffef
Limwonbin <wonbin3366@naver.com>
2023-04-21 12:23:35 +0900

    git rebase merge 충돌 해결

--- Desktop/sporting/sporting_server/src/main/java/shop/mtcoding/sporting_server/topic/user/dto/UserResponse.java
+++ Desktop/sporting/sporting_server/src/main/java/shop/mtcoding/sporting_server/topic/user/dto/UserResponse.java
@@ -1,14 +1,19 @@
 package shop.mtcoding.sporting_server.topic.user.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-import shop.mtcoding.sporting_server.core.util.MyDateUtils;
 import shop.mtcoding.sporting_server.modules.user.entity.User;
 
 public class UserResponse {
 
     @Setter
     @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @EqualsAndHashCode
     public static class UserDetailOutDto {
         private Long id;
         private String email;
