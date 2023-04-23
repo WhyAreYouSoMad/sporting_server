4dfc41ee7be4e1ebb34d8d29b22ae123278c0141
aozp73 <aozp73@naver.com>
2023-04-23 12:55:07 +0900

    GFP-77-S3 연동 관련 Stadium findAllMyList 반영 완료

--- Desktop/sporting/sporting_server/src/main/java/shop/mtcoding/sporting_server/modules/stadium/repository/StadiumCustomRepository.java
+++ Desktop/sporting/sporting_server/src/main/java/shop/mtcoding/sporting_server/modules/stadium/repository/StadiumCustomRepository.java
@@ -6,12 +6,13 @@ import org.springframework.data.repository.query.Param;
 
 import shop.mtcoding.sporting_server.topic.stadium.dto.CourtResponseDTO;
 import shop.mtcoding.sporting_server.topic.stadium.dto.SportCategoryDTO;
-import shop.mtcoding.sporting_server.topic.stadium.dto.StadiumCourtDTO;
 import shop.mtcoding.sporting_server.topic.stadium.dto.StadiumDetailDTO;
+import shop.mtcoding.sporting_server.topic.stadium.dto.StadiumMyListOutDTO;
 import shop.mtcoding.sporting_server.topic.stadium.dto.StadiumUpdateFomrOutDTO;
 
 // QueryDsl 매핑 인터페이스
 public interface StadiumCustomRepository {
+    public List<StadiumMyListOutDTO> findStadiumMyListBySportKeyword(String sportKeyword, Long principalCompanyId);
 
     List<CourtResponseDTO> findCourtsByStadiumId(@Param("stadiumId") Long stadiumId);
 
