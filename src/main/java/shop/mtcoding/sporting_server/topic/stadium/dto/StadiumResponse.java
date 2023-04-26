358f0fa0c6fbce9a70b3c1f1b51ea2f3ff993846
aozp73 <aozp73@naver.com>
2023-04-26 16:20:46 +0900

    GFP-108- 경기장 정보 수정관련 Controller Mock Test 완료

--- src/main/java/shop/mtcoding/sporting_server/topic/stadium/dto/StadiumResponse.java
+++ src/main/java/shop/mtcoding/sporting_server/topic/stadium/dto/StadiumResponse.java
@@ -17,7 +17,6 @@ public class StadiumResponse {
     @Getter
     @Setter
     @NoArgsConstructor
-    @AllArgsConstructor
     @EqualsAndHashCode
     public static class StadiumRegistrationOutDTO {
         private Long id;
@@ -27,6 +26,17 @@ public class StadiumResponse {
         private String startTime;
         private String endTime;
         private StadiumFileDto stadiumFile = new StadiumFileDto();
+
+        public StadiumRegistrationOutDTO(Long id, String name, String address, String category, String startTime,
+                String endTime, StadiumFileDto stadiumFile) {
+            this.id = id;
+            this.name = name;
+            this.address = address;
+            this.category = category;
+            this.startTime = startTime;
+            this.endTime = endTime;
+            this.stadiumFile = stadiumFile;
+        }
 
         public StadiumRegistrationOutDTO(Stadium stadiumPS, ProfileFile profileFile) {
             this.id = stadiumPS.getId();
@@ -64,8 +74,20 @@ public class StadiumResponse {
         private String startTime;
         private String endTime;
         private String category;
-        private StadiumFileDTO stadiumFile = new StadiumFileDTO();
+        private StadiumFileOutDTO stadiumFile = new StadiumFileOutDTO();
         private List<CourtOutDTO> courtList = new ArrayList<>();
+
+        public StadiumUpdateOutDTO(String id, String address, String status, String startTime, String endTime,
+                String category, StadiumFileOutDTO stadiumFile, List<CourtOutDTO> courtList) {
+            this.id = id;
+            this.address = address;
+            this.status = status;
+            this.startTime = startTime;
+            this.endTime = endTime;
+            this.category = category;
+            this.stadiumFile = stadiumFile;
+            this.courtList = courtList;
+        }
 
         public StadiumUpdateOutDTO(Stadium stadiumPS, ProfileFile stadiumFile, List<CourtOutDTO> courtList) {
             this.id = Long.toString(stadiumPS.getId());
@@ -85,7 +107,7 @@ public class StadiumResponse {
         @NoArgsConstructor
         @AllArgsConstructor
         @EqualsAndHashCode
-        public static class StadiumFileDTO {
+        public static class StadiumFileOutDTO {
             private String id;
             private String fileName;
             private String fileUrl;
@@ -94,7 +116,6 @@ public class StadiumResponse {
         @Getter
         @Setter
         @NoArgsConstructor
-
         @EqualsAndHashCode
         public static class CourtOutDTO {
             private String id;
@@ -102,7 +123,17 @@ public class StadiumResponse {
             private String content;
             private String capacity;
             private String courtPrice;
-            private CourtFileDTO courtFile = new CourtFileDTO();
+            private CourtFileOutDTO courtFile = new CourtFileOutDTO();
+
+            public CourtOutDTO(String id, String title, String content, String capacity, String courtPrice,
+                    CourtFileOutDTO courtFile) {
+                this.id = id;
+                this.title = title;
+                this.content = content;
+                this.capacity = capacity;
+                this.courtPrice = courtPrice;
+                this.courtFile = courtFile;
+            }
 
             public CourtOutDTO(StadiumCourt stadiumCourtPS, ProfileFile courtFilePS) {
                 this.id = Long.toString(stadiumCourtPS.getId());
@@ -121,7 +152,7 @@ public class StadiumResponse {
             @NoArgsConstructor
             @AllArgsConstructor
             @EqualsAndHashCode
-            public static class CourtFileDTO {
+            public static class CourtFileOutDTO {
                 private String id;
                 private String fileName;
                 private String fileUrl;
