daf540e172704d390133e22ec28858577736e387
aozp73 <aozp73@naver.com>
2023-04-28 14:15:58 +0900

    Email - Controller에서 email 전송완료까지 확인 완료

--- src/main/java/shop/mtcoding/sporting_server/admin/email/AdminEmailController.java
+++ src/main/java/shop/mtcoding/sporting_server/admin/email/AdminEmailController.java
@@ -3,6 +3,8 @@ package shop.mtcoding.sporting_server.admin.email;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +18,7 @@ import shop.mtcoding.sporting_server.admin.email.dto.EmailDTO.EmailObj;
 @RequestMapping("/admin")
 @Controller
 public class AdminEmailController {
-
+    private final JavaMailSender javaMailSender;
     private AdminEmailService adminEmailService;
 
     @PostMapping("/email")
@@ -29,8 +31,17 @@ public class AdminEmailController {
         System.out.println("테스트 : " + emailList.get(0).getId());
         System.out.println("테스트 : " + emailList.get(0).getEmail());
         System.out.println("==============================================");
-        System.out.println("테스트 : " + emailList.get(1).getId());
-        System.out.println("테스트 : " + emailList.get(1).getEmail());
+        // System.out.println("테스트 : " + emailList.get(1).getId());
+        // System.out.println("테스트 : " + emailList.get(1).getEmail());
+
+        String fromAddress = "aozp83@gmail.com";
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromAddress);
+        message.setTo(emailDTO.getEmailList().get(0).getEmail());
+        message.setSubject(emailDTO.getTitle());
+        message.setText(emailDTO.getContent());
+        javaMailSender.send(message);
         return null;
     }
 }
