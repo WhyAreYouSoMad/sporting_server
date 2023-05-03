4e46b2e0f7dcc3d68b89364653edabedac67cdb2
aozp73 <aozp73@naver.com>
2023-05-03 18:13:06 +0900

    예약 - 나의 예약정보 List 구현 완료

--- src/main/java/shop/mtcoding/sporting_server/modules/court_reservation/repository/CourtReservationRepositoryImpl.java
+++ src/main/java/shop/mtcoding/sporting_server/modules/court_reservation/repository/CourtReservationRepositoryImpl.java
@@ -1,5 +1,36 @@
 package shop.mtcoding.sporting_server.modules.court_reservation.repository;
 
-public class CourtReservationRepositoryImpl {
+import java.util.List;
 
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import lombok.RequiredArgsConstructor;
+import shop.mtcoding.sporting_server.modules.court_reservation.entity.QCourtReservation;
+import shop.mtcoding.sporting_server.topic.user_reservation.dto.QReservationListOutDTO;
+import shop.mtcoding.sporting_server.topic.user_reservation.dto.ReservationListOutDTO;
+
+@Repository
+@RequiredArgsConstructor
+@Transactional
+public class CourtReservationRepositoryImpl implements CourtReservationCustomRepository {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public List<ReservationListOutDTO> findReservationListByUserId(Long principalUserId) {
+        QCourtReservation qCourtReservation = QCourtReservation.courtReservation;
+
+        JPAQuery<ReservationListOutDTO> query = jpaQueryFactory
+                .select(new QReservationListOutDTO(qCourtReservation.id,
+                        qCourtReservation.courtPayment.stadiumCourt.title, qCourtReservation.reservationDate,
+                        qCourtReservation.reservationTime, qCourtReservation.courtPayment.paymentAmount))
+                .from(qCourtReservation)
+                .where(qCourtReservation.user.id.eq(principalUserId));
+
+        return query.fetch();
+    }
 }
--- src/main/java/shop/mtcoding/sporting_server/topic/user_reservation/UserResService.java
+++ src/main/java/shop/mtcoding/sporting_server/topic/user_reservation/UserResService.java
@@ -1,8 +1,22 @@
 package shop.mtcoding.sporting_server.topic.user_reservation;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+import shop.mtcoding.sporting_server.modules.court_reservation.repository.CourtReservationRepository;
+import shop.mtcoding.sporting_server.topic.user_reservation.dto.ReservationListOutDTO;
+
+@RequiredArgsConstructor
 @Service
 public class UserResService {
+
+    private final CourtReservationRepository courtReservationRepository;
+
+    public List<ReservationListOutDTO> getReservationList(Long id) {
+
+        return courtReservationRepository.findReservationListByUserId(id);
+    }
 
 }
