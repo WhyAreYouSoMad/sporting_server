797cc31a66f9f281a07b25a566042863f63a443c
aozp73 <aozp73@naver.com>
2023-05-03 13:56:26 +0900

    결제관련 reciptDTO 구조 변경 완료

--- src/main/java/shop/mtcoding/sporting_server/topic/payment/dto/PaymentRequest.java
+++ src/main/java/shop/mtcoding/sporting_server/topic/payment/dto/PaymentRequest.java
@@ -33,105 +33,119 @@ public class PaymentRequest {
     @AllArgsConstructor
     @NoArgsConstructor
     public static class ReceiptInDTO {
-        @JsonProperty("receipt_id")
-        private String receiptId;
-
-        @JsonProperty("order_id")
-        private String orderId;
-
-        @JsonProperty("price")
-        private int price;
-
-        @JsonProperty("tax_free")
-        private int taxFree;
-
-        @JsonProperty("cancelled_price")
-        private int cancelledPrice;
-
-        @JsonProperty("cancelled_tax_free")
-        private int cancelledTaxFree;
-
-        @JsonProperty("order_name")
-        private String orderName;
-
-        @JsonProperty("company_name")
-        private String companyName;
-
-        @JsonProperty("gateway_url")
-        private String gatewayUrl;
-
-        @JsonProperty("metadata")
-        private Metadata metadata;
-
-        private boolean sandbox;
-
-        private String pg;
-
-        private String method;
-
-        @JsonProperty("method_symbol")
-        private String methodSymbol;
-
-        @JsonProperty("method_origin")
-        private String methodOrigin;
-
-        @JsonProperty("method_origin_symbol")
-        private String methodOriginSymbol;
-
-        @JsonProperty("purchased_at")
-        private OffsetDateTime purchasedAt;
-
-        @JsonProperty("cancelled_at")
-        private OffsetDateTime cancelledAt;
-
-        @JsonProperty("requested_at")
-        private OffsetDateTime requestedAt;
-
-        @JsonProperty("status_locale")
-        private String statusLocale;
-
-        @JsonProperty("receipt_url")
-        private String receiptUrl;
-
-        private int status;
-
-        @JsonProperty("kakao_money_data")
-        private CardData kakaoMoneyData;
+        @JsonProperty("event")
+        private String event;
+        @JsonProperty("data")
+        private ReceiptDataDTO data;
+        @JsonProperty("bootpay_event")
+        private String bootpayEvent;
 
         @Getter
         @Setter
         @AllArgsConstructor
         @NoArgsConstructor
-        public class CardData {
-            @JsonProperty("tid")
-            private String tid;
+        public static class ReceiptDataDTO {
+            @JsonProperty("receipt_id")
+            private String receiptId;
 
-            @JsonProperty("cancel_tid")
-            private String cancelTid;
+            @JsonProperty("order_id")
+            private String orderId;
 
-            // @JsonProperty("card_approve_no")
-            // private String cardApproveNo;
+            @JsonProperty("price")
+            private int price;
 
-            // @JsonProperty("card_no")
-            // private String cardNo;
+            @JsonProperty("tax_free")
+            private int taxFree;
 
-            // @JsonProperty("card_quota")
-            // private String cardQuota;
+            @JsonProperty("cancelled_price")
+            private int cancelledPrice;
 
-            // @JsonProperty("card_company_code")
-            // private String cardCompanyCode;
+            @JsonProperty("cancelled_tax_free")
+            private int cancelledTaxFree;
 
-            // @JsonProperty("card_company")
-            // private String cardCompany;
+            @JsonProperty("order_name")
+            private String orderName;
+
+            @JsonProperty("company_name")
+            private String companyName;
+
+            @JsonProperty("gateway_url")
+            private String gatewayUrl;
+
+            @JsonProperty("metadata")
+            private Metadata metadata;
+
+            private boolean sandbox;
+
+            private String pg;
+
+            private String method;
+
+            @JsonProperty("method_symbol")
+            private String methodSymbol;
+
+            @JsonProperty("method_origin")
+            private String methodOrigin;
+
+            @JsonProperty("method_origin_symbol")
+            private String methodOriginSymbol;
+
+            @JsonProperty("purchased_at")
+            private OffsetDateTime purchasedAt;
+
+            @JsonProperty("cancelled_at")
+            private OffsetDateTime cancelledAt;
+
+            @JsonProperty("requested_at")
+            private OffsetDateTime requestedAt;
+
+            @JsonProperty("status_locale")
+            private String statusLocale;
+
+            @JsonProperty("receipt_url")
+            private String receiptUrl;
+
+            private int status;
+
+            @JsonProperty("kakao_money_data")
+            private CardData kakaoMoneyData;
+
+            @Getter
+            @Setter
+            @AllArgsConstructor
+            @NoArgsConstructor
+            public class CardData {
+                @JsonProperty("tid")
+                private String tid;
+
+                @JsonProperty("cancel_tid")
+                private String cancelTid;
+
+                // @JsonProperty("card_approve_no")
+                // private String cardApproveNo;
+
+                // @JsonProperty("card_no")
+                // private String cardNo;
+
+                // @JsonProperty("card_quota")
+                // private String cardQuota;
+
+                // @JsonProperty("card_company_code")
+                // private String cardCompanyCode;
+
+                // @JsonProperty("card_company")
+                // private String cardCompany;
+            }
+
+            @Getter
+            @Setter
+            @JsonInclude(JsonInclude.Include.NON_NULL)
+            public class Metadata {
+                // @JsonProperty("test")
+                // private String test;
+            }
         }
 
-        @Getter
-        @Setter
-        @JsonInclude(JsonInclude.Include.NON_NULL)
-        public class Metadata {
-            // @JsonProperty("test")
-            // private String test;
-        }
     }
 
 }
