626984c9ecf23eb74b61d180974b3971e9112a00
Limwonbin <wonbin3366@naver.com>
2023-05-07 03:49:41 +0900

    stadiumCourt 통합테스트 완료

---
+++ src/main/java/shop/mtcoding/sporting_server/core/dummy/StadiumCourtResDummy.java
@@ -0,0 +1,174 @@
+package shop.mtcoding.sporting_server.core.dummy;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import shop.mtcoding.sporting_server.core.enums.field.etc.FileInfoSource;
+import shop.mtcoding.sporting_server.core.enums.field.status.CourtPaymentStatus;
+import shop.mtcoding.sporting_server.core.enums.field.status.CourtReservationStatus;
+import shop.mtcoding.sporting_server.core.enums.field.status.StadiumCourtStatus;
+import shop.mtcoding.sporting_server.core.enums.field.status.StadiumStatus;
+import shop.mtcoding.sporting_server.core.enums.field.status.UserStatus;
+import shop.mtcoding.sporting_server.modules.company_info.entity.CompanyInfo;
+import shop.mtcoding.sporting_server.modules.court_payment.entity.CourtPayment;
+import shop.mtcoding.sporting_server.modules.court_reservation.entity.CourtReservation;
+import shop.mtcoding.sporting_server.modules.file.entity.ProfileFile;
+import shop.mtcoding.sporting_server.modules.fileinfo.entity.FileInfo;
+import shop.mtcoding.sporting_server.modules.player_info.entity.PlayerInfo;
+import shop.mtcoding.sporting_server.modules.sport_category.entity.SportCategory;
+import shop.mtcoding.sporting_server.modules.stadium.entity.Stadium;
+import shop.mtcoding.sporting_server.modules.stadium_court.entity.StadiumCourt;
+import shop.mtcoding.sporting_server.modules.user.entity.User;
+
+public class StadiumCourtResDummy {
+    // User
+    public User newPlayerUser(String username, String nickname) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+        return User.builder()
+                .nickname(nickname)
+                .email(username + "@nate.com")
+                .password(passwordEncoder.encode("1234"))
+                .role("PLAYER")
+                .status(UserStatus.일반회원)
+                .build();
+    }
+
+    public User newCompanyUser(String username, String nickname) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+        return User.builder()
+                .nickname(nickname)
+                .email(username + "@nate.com")
+                .password(passwordEncoder.encode("1234"))
+                .role("COMPNAY")
+                .status(UserStatus.기업회원)
+                .build();
+    }
+
+    // Info
+    public CompanyInfo newCompanyInfo(User user, FileInfo fileInfo) {
+
+        return CompanyInfo.builder()
+                .user(user)
+                .businessNumber(null)
+                .businessAddress(null)
+                .tel(null)
+                .ceo(null)
+                .fileInfo(fileInfo)
+                .updatedAt(LocalDateTime.now())
+                .build();
+    }
+
+    public PlayerInfo newPlayerInfo(User user, FileInfo fileInfo) {
+
+        return PlayerInfo.builder()
+                .user(user)
+                .gender(null)
+                .age(null)
+                .address(null)
+                .tel(null)
+                .fileInfo(fileInfo)
+                .updatedAt(LocalDateTime.now())
+                .build();
+    }
+
+    // ProfileFile, FileInfo
+    public ProfileFile newProfileFile(FileInfo fileInfo) {
+
+        return ProfileFile.builder()
+                .fileInfo(fileInfo)
+                .fileName("CompanyProfile/company.jpg")
+                .fileUrl("https://3-sporting.s3.ap-northeast-2.amazonaws.com/CompanyProfile/company.jpg")
+                .build();
+    }
+
+    public FileInfo newFileInfo(FileInfoSource fileInfoSource) {
+
+        return FileInfo.builder().type(fileInfoSource).build();
+    }
+
+    // Stadium, Court
+    public Stadium newStadium(CompanyInfo companyInfo, SportCategory sportCategory) {
+
+        return Stadium
+                .builder()
+                .companyInfo(companyInfo)
+                .name("테스트 경기장")
+                .address("부산시 연제구")
+                .category(sportCategory)
+                .status(StadiumStatus.운영중)
+                .startTime(LocalTime.of(9, 0))
+                .endTime(LocalTime.of(18, 0))
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+    public StadiumCourt newStadiumCourt(Stadium stadium, FileInfo fileInfo) {
+
+        return StadiumCourt.builder()
+                .stadium(stadium)
+                .fileInfo(fileInfo)
+                .title("코트제목(통합테스트)")
+                .content("코트내용(통합테스트)")
+                .capacity(10)
+                .courtPrice(40000)
+                .createdAt(LocalDateTime.now())
+                .status(StadiumCourtStatus.등록대기)
+                .build();
+    }
+
+    // SportCategory
+    public SportCategory newSportCategory() {
+
+        return SportCategory
+                .builder()
+                .sport("축구")
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+    // Payment, Reservation
+    public CourtPayment newCourtPayment(PlayerInfo playerInfo, CompanyInfo companyInfo, StadiumCourt stadiumCourt,
+            Integer price) throws JsonProcessingException {
+
+        ObjectMapper om = new ObjectMapper();
+
+        return CourtPayment
+                .builder()
+                .paymentType("card")
+                .originData(om.writeValueAsString("originData"))
+                .playerInfo(playerInfo)
+                .companyInfo(companyInfo)
+                .paymentAmount(price)
+                .stadiumCourt(stadiumCourt)
+                .receiptId("RC12345")
+                .status(CourtPaymentStatus.결제완료)
+                .purchasedAt(LocalDateTime.now())
+                .requestedAt(LocalDateTime.now())
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+    public CourtReservation newCourtReservation(PlayerInfo playerInfo, String resTime, CourtPayment courtPayment,
+            StadiumCourt stadiumCourt) throws JsonProcessingException {
+
+        return CourtReservation
+                .builder()
+                .user(playerInfo.getUser())
+                .reservationDate(LocalDate.now())
+                .reservationTime(resTime)
+                .courtPayment(courtPayment)
+                .stadiumCourt(stadiumCourt)
+                .createdAt(LocalDateTime.now())
+                .status(CourtReservationStatus.승낙)
+                .build();
+    }
+
+}
--- src/test/java/shop/mtcoding/sporting_server/integrated/StadiumCourtControllerTest.java
+++ src/test/java/shop/mtcoding/sporting_server/integrated/StadiumCourtControllerTest.java
@@ -1,20 +1,117 @@
 package shop.mtcoding.sporting_server.integrated;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import javax.persistence.EntityManager;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import shop.mtcoding.sporting_server.AbstractControllerTest;
+import shop.mtcoding.sporting_server.core.auth.MyUserDetails;
+import shop.mtcoding.sporting_server.core.dummy.StadiumCourtResDummy;
+import shop.mtcoding.sporting_server.core.enums.field.etc.FileInfoSource;
+import shop.mtcoding.sporting_server.core.jwt.MyJwtProvider;
+import shop.mtcoding.sporting_server.modules.company_info.entity.CompanyInfo;
+import shop.mtcoding.sporting_server.modules.company_info.repository.CompanyInfoRepository;
+import shop.mtcoding.sporting_server.modules.file.repository.ProfileFileRepository;
+import shop.mtcoding.sporting_server.modules.fileinfo.entity.FileInfo;
+import shop.mtcoding.sporting_server.modules.fileinfo.repository.FileInfoRepository;
+import shop.mtcoding.sporting_server.modules.sport_category.entity.SportCategory;
+import shop.mtcoding.sporting_server.modules.sport_category.repository.SportCategoryRepository;
+import shop.mtcoding.sporting_server.modules.stadium.entity.Stadium;
+import shop.mtcoding.sporting_server.modules.stadium.repository.StadiumRepository;
+import shop.mtcoding.sporting_server.modules.user.entity.User;
+import shop.mtcoding.sporting_server.modules.user.repository.UserRepository;
+import shop.mtcoding.sporting_server.topic.stadium_court.dto.StadiumCourtRequest;
 
 @DisplayName("경기장 코트 API")
 @AutoConfigureRestDocs(uriScheme = "http", uriHost = "localhost", uriPort = 8080)
 @ActiveProfiles("test")
 @Sql("classpath:db/teardown.sql")
 @AutoConfigureMockMvc
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 public class StadiumCourtControllerTest extends AbstractControllerTest {
+    protected MockMvc mockMvc;
+    protected final ObjectMapper objectMapper = new ObjectMapper();
+    private StadiumCourtResDummy dummy = new StadiumCourtResDummy();
 
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private ObjectMapper om;
+    @Autowired
+    private EntityManager em;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private FileInfoRepository fileInfoRepository;
+    @Autowired
+    private ProfileFileRepository profileFileRepository;
+    @Autowired
+    private CompanyInfoRepository companyInfoRepository;
+    @Autowired
+    private SportCategoryRepository sportCategoryRepository;
+    @Autowired
+    private StadiumRepository stadiumRepository;
+    @MockBean
+    private MyUserDetails myUserDetails;
+
+    @BeforeEach
+    public void setUp() {
+        User companyUser = userRepository.save(dummy.newCompanyUser("cos", "cos"));
+        FileInfo fileInfo = fileInfoRepository.save(dummy.newFileInfo(FileInfoSource.기업프로필));
+        profileFileRepository.save(dummy.newProfileFile(fileInfo));
+        CompanyInfo companyInfo = companyInfoRepository.save(dummy.newCompanyInfo(companyUser, fileInfo));
+
+        SportCategory sportCategory = sportCategoryRepository.save(dummy.newSportCategory());
+        stadiumRepository.save(dummy.newStadium(companyInfo, sportCategory));
+        em.clear();
+    }
+
+    @DisplayName("Stadium Court 등록")
+    @Test
+    void stadium_court_save_test() throws Exception {
+        // given
+        Long stadiumId = 1L;
+        String jwt = MyJwtProvider.create(User.builder().id(1L).nickname("cos").role("COMPANY").build());
+        StadiumCourtRequest.StadiumCourtInDTO stadiumCourtInDTO = StadiumCourtRequest.StadiumCourtInDTO
+                .builder()
+                .courtProfile("")
+                .title("테스트 제목")
+                .content("테스트 내용")
+                .capacity(10)
+                .courtPrice(30000)
+                .build();
+        String requestBody = om.writeValueAsString(stadiumCourtInDTO);
+
+        // when
+        ResultActions resultActions = mvc
+                .perform(post("/api/company/stadiums/" + stadiumId + "/court")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header(MyJwtProvider.HEADER, MyJwtProvider.TOKEN_PREFIX + jwt));
+
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody + "테스트 끝");
+
+        // then
+        resultActions.andExpect(status().isOk());
+        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
 }
