77397796a838d892ec4fa553e7b2f44dc37b8e4c
aozp73 <aozp73@naver.com>
2023-05-09 13:44:25 +0900

    CICD 관련하여 코드 수정 완료

--- src/main/java/shop/mtcoding/sporting_server/SportingServerApplication.java
+++ src/main/java/shop/mtcoding/sporting_server/SportingServerApplication.java
@@ -3,8 +3,6 @@ package shop.mtcoding.sporting_server;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class SportingServerApplication {
@@ -13,20 +11,22 @@ public class SportingServerApplication {
 		SpringApplication.run(SportingServerApplication.class, args);
 	}
 
-	@Profile("dev")
-	@Bean
-	public CommandLineRunner initData1() {
-		return (args) -> {
-			System.out.println("Dev CommandLineRunner 실행됨 ------------------------------------------");
-		};
-	}
+	// @Profile("dev")
+	// @Bean
+	// public CommandLineRunner initData1() {
+	// return (args) -> {
+	// System.out.println("Dev CommandLineRunner 실행됨
+	// ------------------------------------------");
+	// };
+	// }
 
-	// 스프링서버가 최초 실행될 때 단 한번 실행되는 배치 메서드이다. prod 모드일때만 작동한다.
-	@Profile("prod")
-	@Bean
-	public CommandLineRunner initData3() {
-		return (args) -> {
-			System.out.println("Prod CommandLineRunner 실행됨 ------------------------------------------");
-		};
-	}
+	// // 스프링서버가 최초 실행될 때 단 한번 실행되는 배치 메서드이다. prod 모드일때만 작동한다.
+	// @Profile("prod")
+	// @Bean
+	// public CommandLineRunner initData3() {
+	// return (args) -> {
+	// System.out.println("Prod CommandLineRunner 실행됨
+	// ------------------------------------------");
+	// };
+	// }
 }
