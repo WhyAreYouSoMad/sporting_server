71d5db3a727043a29bd5acf2ff670536053c8ed5
aozp73 <aozp73@naver.com>
2023-05-09 12:38:06 +0900

    CICD TEST 3-14

--- src/main/java/shop/mtcoding/sporting_server/topic/board/BoardController.java
+++ src/main/java/shop/mtcoding/sporting_server/topic/board/BoardController.java
@@ -1,5 +1,7 @@
 package shop.mtcoding.sporting_server.topic.board;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
